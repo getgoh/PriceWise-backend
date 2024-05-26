f35505aed6257286525d952c3ab5116b782014c7
GetGoh <agohca@gmail.com>
2024-05-26 18:43:25 -0400

    Added function to create product using POST method. #2

--- src/main/kotlin/com/xalles/pricewise/products/ProductController.kt
+++ src/main/kotlin/com/xalles/pricewise/products/ProductController.kt
@@ -3,10 +3,7 @@ package com.xalles.pricewise.products
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 // Rest API
 
@@ -24,4 +21,11 @@ class ProductController(@Autowired private val productRepository: ProductReposit
         return if (product != null) ResponseEntity<Product>(product, HttpStatus.OK)
             else ResponseEntity(HttpStatus.NOT_FOUND)
     }
+
+    @PostMapping
+    fun createProduct(@RequestBody product: Product): ResponseEntity<Product> {
+        val createdProduct = productRepository.save(product)
+        return ResponseEntity<Product>(createdProduct, HttpStatus.CREATED)
+    }
+
 }

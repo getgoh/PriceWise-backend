86bf0542f92bcf3b197a982f2bbcfc19be8d3a74
GetGoh <agohca@gmail.com>
2024-05-26 16:25:17 -0400

    Adding starting project files

---
+++ src/main/kotlin/com/xalles/pricewise/PriceWiseApplication.kt
@@ -0,0 +1,11 @@
+package com.xalles.pricewise
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PriceWiseApplication
+
+fun main(args: Array<String>) {
+    runApplication<PriceWiseApplication>(*args)
+}
